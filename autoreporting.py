a583663a6d4b3d470af44852195619ee18b86868
Matthew Oehler <matthew.oehler@progleasing.com>
2019-08-16 08:42:10 -0600

    added a basic image viewing template.

--- autoreporting.py
+++ autoreporting.py
@@ -2,6 +2,23 @@
 
 #libraries
 from jinja2 import FileSystemLoader, Environment
+import pandas as pd
+# import seaborn as sns
+# from matplotlib import pyplot as plt
+
+#configure display settings for pandas tables (enable wide columns)
+pd.set_option("display.max_colwidth",200)
+
+#function to turn csv file into html table
+def csv_to_html(file_path, nrows = -1):
+    """read in a csv file and convert it to an html string
+    """
+    df = pd.read_csv(file_path)
+    if nrows > 0:
+        df = df.head(nrows)
+    html_output = df.to_html(index = False)
+    return html_output
+
 
 #configure the Jinja and set up the template
 env = Environment(
@@ -11,19 +28,7 @@ env = Environment(
 #template that we will use
 base_template = env.get_template("report.html")
 table_section_template = env.get_template("table_section.html")
-
-
-#content to be published
-#content = [1,2,3,4,5]#"This is my first report"
-title = "Report with a table section"
-sections = list()
-
-sections.append(table_section_template.render(
-    table_title = "Pclass Pivot",
-    file_name = "pclasspiv.csv",
-    table = "Table goes here"
-))
-
+image_section_template = env.get_template("image_section.html")
 
 
 
@@ -32,6 +37,31 @@ def main():
     """
     main function: renders the template and writes it to a file
     """
+
+    #content to be published
+    #content = [1,2,3,4,5]#"This is my first report"
+    title = "Report with a table section"
+    sections = list()
+
+
+    #modify templates in main function
+    sections.append(table_section_template.render(
+        table_title = "First few rows of the titanic data set",
+        file_name = "train.csv",
+        table = csv_to_html("data_files/train.csv", nrows = 5)
+    ))
+
+    sections.append(table_section_template.render(
+        table_title = "Pclass Pivot",
+        file_name = "pclass_piv.csv",
+        table = csv_to_html("data_files/pclass_piv.csv")
+    ))
+
+    sections.append(image_section_template.render(
+        image_title = "Pclass by Gender",
+        file_name = "pclass_gender_chart.jpg",
+        image = "../images/pclass_gender_chart.jpg"
+    ))
 
     #open or generate the report file and add the content
     with open("outputs/report.html", "w") as f:
