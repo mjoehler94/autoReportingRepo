bc08df0bf5a071fa882307b2193a23bb454ad27e
Matthew Oehler <matthew.oehler@progleasing.com>
2019-08-16 07:26:22 -0600

    added sections to base_template, next step is to add a table from datafile

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Automated Reporting
 
 ### The goal of this project is to help me to understand how to make reports automatically using python
+
+Acknowledgements: goyder wrote a stellar series of blog posts, the first of which is [here](https://dev.to/goyder/automatic-reporting-in-python---part-1-from-planning-to-hello-world-32n1)
--- autoreporting.py
+++ autoreporting.py
@@ -3,15 +3,27 @@
 #libraries
 from jinja2 import FileSystemLoader, Environment
 
-#content to be published
-content = "This is my first report"
-
 #configure the Jinja and set up the template
 env = Environment(
         loader=FileSystemLoader(searchpath="templates")
-)   
+)
 
-template = env.get_template("report.html")
+#template that we will use
+base_template = env.get_template("report.html")
+table_section_template = env.get_template("table_section.html")
+
+
+#content to be published
+#content = [1,2,3,4,5]#"This is my first report"
+title = "Report with a table section"
+sections = list()
+
+sections.append(table_section_template.render(
+    table_title = "Pclass Pivot",
+    file_name = "pclasspiv.csv",
+    table = "Table goes here"
+))
+
 
 
 
@@ -23,11 +35,12 @@ def main():
 
     #open or generate the report file and add the content
     with open("outputs/report.html", "w") as f:
-        f.write(template.render(content=content))
-
+        f.write(base_template.render(
+            title=title,
+            sections = sections
+        ))
 
 #run the script!
 if __name__ == "__main__":
     main()
     print("Done.")
-
